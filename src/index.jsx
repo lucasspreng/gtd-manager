450ee87d610a4b57a09914e62262e68b17368ac3
Lucas Spreng <lucasspreng@gmail.com>
2020-11-20 00:03:16 -0300

    Modelo inicial com Material UI

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import {
+	Login,
+	Home,
+} from './screens';
+
+const Router = createStackNavigator(
+	{
+		Login,
+		Home,
+	},
+	{
+		initialRouteName: 'Login',
+		headerMode: 'none',
+	}
+);
+
+export default createAppContainer(Router);
